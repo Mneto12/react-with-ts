710f70523a5d665ed98c8c67643d9c5c80171d01
Miguel Coronel Rivas <envenezuela2000@gmail.com>
2023-03-07 20:30:38 -0800

    add lazyLoading logic and make generic comp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { RandomFox } from "@/components/RandomFox"
-import { useState } from "react";
+import { MouseEventHandler, useState } from "react";
 
 const randomN = (): number => Math.floor(Math.random() * 122) + 1;
 
@@ -9,7 +9,6 @@ type ImageItem = {
   url: string
 }
 
-// generate simple unique id
 const generateId = (): string => {
   return (
     Math.random().toString(36).substring(2, 15) +
@@ -17,22 +16,37 @@ const generateId = (): string => {
   )
 }
 
+const ButtonClass: string = "focus:outline-none text-white bg-purple-700 mt-4 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+
+const H1Class: string = "mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
+
 
 export default function Home() {
 
-  const [Images, setImages] = useState<Array<ImageItem>>([
-    {id: generateId(), url:`https://randomfox.ca/images/${randomN()}.jpg`},
-    {id: generateId(), url:`https://randomfox.ca/images/${randomN()}.jpg`},
-    {id: generateId(), url:`https://randomfox.ca/images/${randomN()}.jpg`},
-    {id: generateId(), url:`https://randomfox.ca/images/${randomN()}.jpg`},
-  ])
+  const [Images, setImages] = useState<Array<ImageItem>>([])
+
+  const addNewFox: MouseEventHandler<HTMLButtonElement> = () => {
+
+
+    const newImage: ImageItem = {
+      id: generateId(),
+      url: `https://randomfox.ca/images/${randomN()}.jpg`
+    }
+    setImages([...Images, newImage])
+  }
 
   return (
     <main>
-      <div>
+      <div className="mt-4 w-2/4 mx-auto text-center">
+        <h1 className={H1Class}>Random Foxes</h1>
+        <button className={ButtonClass} onClick={addNewFox}>Add new fox</button>
           {Images.map(({id,url}) => (
-            <div className="p-4" key={id}>
-              <RandomFox image={url}/>
+            <div className="p-4 h-auto" key={id}>
+              <RandomFox 
+                src={url} 
+                className="mx-auto rounded-lg h-60 w-60" 
+                alt="Random Fox"
+              />
             </div>
           ))}
       </div>
--- src/components/RandomFox.tsx
+++ src/components/RandomFox.tsx
@@ -1,12 +1,39 @@
+import { useRef, useEffect, useState, ImgHTMLAttributes } from 'react';
+
 type props = {
-    image: string
+    src: string
 }
 
-export const RandomFox = ({image}: props): JSX.Element => {
+type Props = ImgHTMLAttributes<HTMLImageElement> & props
 
+export const RandomFox = ({src, ...imgpProps}: Props): JSX.Element => {
+
+    const node = useRef<HTMLImageElement>(null)
+    const [Currentsrc, setCurrentsrc] = useState("https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif")
+
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (!entry.isIntersecting || !node.current) {
+                    return;
+                }
+                setCurrentsrc(src)
+            })
+        })
+
+        if (node.current) {
+            observer.observe(node.current)
+        }
+
+        return () => {
+            observer.disconnect()
+        }
+    }, [src])
+
+    
     return <img 
-    src={image}
-    className="mx-auto" 
-    alt="Random Fox" 
+    ref={node}
+    src={Currentsrc}
+    {...imgpProps}
     />
 };
